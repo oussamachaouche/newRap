77abdc789105c4b5a661012401fd81f49e321d52
oussamachaouche <oussamachaouch68@gmail.com>
2024-02-21 12:50:59 +0100

    second commit

--- config/admin.js
+++ config/admin.js
@@ -2,16 +2,17 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
-  apiToken: {
-    salt: env('API_TOKEN_SALT'),
-  },
-  transfer: {
-    token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
-    },
-  },
-  flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
-  },
+  url:"/home",
+  // apiToken: {
+  //   salt: env('API_TOKEN_SALT'),
+  // },
+  // transfer: {
+  //   token: {
+  //     salt: env('TRANSFER_TOKEN_SALT'),
+  //   },
+  // },
+  // flags: {
+  //   nps: env.bool('FLAG_NPS', true),
+  //   promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+  // },
 });
